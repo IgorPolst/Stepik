20a985f0118f830c7a4b4625b8ef40b34d9d38ac
IgorPolst <71287351+IgorPolst@users.noreply.github.com>
2024-10-13 00:03:15 +0300

    Возвращение

--- Level_profi/Work_with_file/CSV/step_03.py
+++ Level_profi/Work_with_file/CSV/step_03.py
@@ -1,4 +1,9 @@
 import csv
 with open("Work_with_file/CSV/sales.csv", encoding = "UTF-8") as file: 
     sales = list(csv.reader(file, delimiter=";"))
-    print(*list(map(lambda name: name[0], filter(lambda sale: int(sale[2]) < int(sale[1]),sales[1:]))), sep = "\n")
+    print(
+        *list(
+            map(lambda name: name[0], filter(lambda sale: int(sale[2]) < int(sale[1]),sales[1:])
+                )
+            ), 
+        sep = "\n")
