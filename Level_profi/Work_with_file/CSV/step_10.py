42811e58cee7eaed1b3ebaf669a847e3c9246a06
IgorPolst <igorpolstianoy@gmail.com>
2025-01-20 15:41:24 +0300

    Маленький шаг после большого перерыва)

--- Level_profi/Work_with_file/CSV/step_10.py
+++ Level_profi/Work_with_file/CSV/step_10.py
@@ -2,13 +2,17 @@ import csv
 
 
 unik = dict()
-with open("Work_with_file/CSV/name_log.csv", "rt", encoding="UTF-8") as file:
+with open("Stepik/Level_profi/Work_with_file/CSV/name_log.csv", "rt", encoding="UTF-8") as file:
     new_name = sorted(csv.DictReader(file, delimiter=","), key = lambda email: email["dtime"], reverse= True)
 
 for name in new_name:
     unik[name["email"]] = unik.setdefault(name["email"], [])
     unik[name["email"]].append(name)
 
-    
+new_unik = list(sorted( (map(lambda people: people[0], unik.values())), key= lambda email: email["email"],))
 
-print(*unik.values(), sep = "\n")
+
+with open("Stepik/Level_profi/Work_with_file/CSV/new_name_log.csv", "wt", encoding="UTF-8") as file:
+    write = csv.DictWriter(file, new_unik[0].keys(), delimiter=",", lineterminator='\n')
+    write.writeheader()
+    write.writerows(new_unik)
