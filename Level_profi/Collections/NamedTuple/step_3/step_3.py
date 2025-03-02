226c95131080315bb366bc89ed85707534e05c37
IgorPolst <71287351+IgorPolst@users.noreply.github.com>
2025-03-02 05:54:43 +0300

    nameTuple и начало defaultDict

---
+++ Level_profi/Collections/NamedTuple/step_3/step_3.py
@@ -0,0 +1,12 @@
+from collections import namedtuple
+import pickle
+
+Animal = namedtuple('Animal', ['name', 'family', 'sex', 'color'])
+
+with open("Level_profi/Collections/NamedTuple/step_3/data.pkl", "rb") as file:
+    data = pickle.load(file)
+
+for animal in data:
+    for key, value in animal._asdict().items():
+        print(f"{key}: {value}")
+    print()
